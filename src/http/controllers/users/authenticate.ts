ac28f3a7c4c4b9ae14296370f9e8e1463ef84a0d
leonardo.reich <engsoft.leonardo@gmail.com>
2024-10-26 09:19:56 -0300

    refresh token and RBAC

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -1,12 +1,16 @@
+
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
-import { FastifyRequest, FastifyReply } from 'fastify'
-import { z } from "zod"
 
-export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+export async function authenticate(
+   request: FastifyRequest,
+   reply: FastifyReply,
+) {
    const authenticateBodySchema = z.object({
       email: z.string().email(),
-      password: z.string().min(6)
+      password: z.string().min(6),
    })
 
    const { email, password } = authenticateBodySchema.parse(request.body)
@@ -16,24 +20,48 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
       const { user } = await authenticateUseCase.execute({
          email,
-         password
+         password,
       })
 
-      const token = await reply.jwtSign({}, {
-         sign: {
-            sub: user.id
-         }
-      })
+      const token = await reply.jwtSign(
+         {
+            role: user.role,
+         },
+         {
+            sign: {
+               sub: user.id,
+            },
+         },
+      )
 
-      return reply.status(200).send({
-         token
-      })
-   }
-   catch(err) {
-      if(err instanceof InvalidCredentialsError) {
+      const refreshToken = await reply.jwtSign(
+         {
+            role: user.role,
+         },
+         {
+            sign: {
+               sub: user.id,
+               expiresIn: '7d',
+            },
+         },
+      )
+
+      return reply
+         .setCookie('refreshToken', refreshToken, {
+            path: '/',
+            secure: true,
+            sameSite: true,
+            httpOnly: true,
+         })
+         .status(200)
+         .send({
+            token,
+         })
+   } catch (err) {
+      if (err instanceof InvalidCredentialsError) {
          return reply.status(400).send({ message: err.message })
       }
 
       throw err
-   }   
+   }
 }
